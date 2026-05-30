4104759e054db0b3e7fee5280bdd42c8a93ed583
yigitboi07 <yigitamaprime@gmail.com>
2026-05-30 22:35:30 +0300

    Update Mcaraclar.ps1

--- Mcaraclar.ps1
+++ Mcaraclar.ps1
@@ -113,31 +113,38 @@ $urls = @(
     'https://github.com/winsiderss/si-builds/releases/download/3.2.25275.112/systeminformer-build-canary-setup.exe',
     'https://www.nirsoft.net/utils/usbdeview-x64.zip',
     'https://www.nirsoft.net/utils/networkusageview-x64.zip',
-    'https://www.nirsoft.net/utils/cprocess.zip',
-    'https://github.com/kacos2000/Win10LiveInfo/releases/download/v.1.0.23.0/WinLiveInfo.exe',
-    'https://www.nirsoft.net/utils/appcrashview.zip',
-    'https://www.nirsoft.net/utils/uninstallview-x64.zip',
     'https://d1kpmuwb7gvu1i.cloudfront.net/AccessData_FTK_Imager_4.7.1.exe',
+    'https://github.com/Yamato-Security/hayabusa/releases/download/v3.6.0/hayabusa-3.6.0-win-x64.zip',
     'https://download.ericzimmermanstools.com/net9/TimelineExplorer.zip',
     'https://www.nirsoft.net/utils/usbdrivelog.zip',
     'https://www.voidtools.com/Everything-1.4.1.1029.x64-Setup.exe',
     'https://www.nirsoft.net/utils/previousfilesrecovery-x64.zip',
     'https://github.com/Col-E/Recaf/releases/download/2.21.14/recaf-2.21.14-J8-jar-with-dependencies.jar',
-    'https://github.com/NotRequiem/InjGen/releases/download/v2.0/InjGen.exe',
+    'https://github.com/Orbdiff/InjGen/releases/download/fork/InjGen.exe',
     'https://github.com/ItzIceHere/RedLotus-Mod-Analyzer/releases/download/RL/RedLotusModAnalyzer.exe',
     'https://github.com/RedLotus-Development/White-Lotus-Scanner/releases/download/forensics/WhiteLotus.exe',
+    'https://download.ericzimmermanstools.com/net9/MFTECmd.zip',
+    'https://download.ericzimmermanstools.com/net9/MFTExplorer.zip',
     'https://github.com/zedoonvm1/TasksParser/releases/download/1.1/Tasks.Parser.exe',
+    'https://download.ericzimmermanstools.com/net9/PECmd.zip',
     'https://download.ericzimmermanstools.com/net9/JumpListExplorer.zip',
-    'https://github.com/Orbdiff/Fileless/releases/download/v1.1/Fileless.exe'
-    'https://github.com/txvch/Screenshare-Collector/releases/download/tech/Technical.Utilities.exe'
-    'https://github.com/ItzIceHere/RedLotusAltChecker/releases/download/RL/RedLotusAltChecker.exe'
-    'https://dl.echo.ac/tool/journal'
-    'https://github.com/kacos2000/Win10LiveInfo/releases/download/v.1.0.23.0/WinLiveInfo.exe'
-    'https://www.nirsoft.net/utils/regscanner.html'
-    'https://github.com/moaistory/WinSearchDBAnalyzer/releases/download/1.0.0.6/WinSearchDBAnalyzer.exe'
-    'https://www.nirsoft.net/utils/appaudioconfig-x64.zip'
-    'https://github.com/zodiacon/AllTools/raw/master/NtfsStreams.zip'
-    'https://api.anticheat.ac/dl/cli'
+    'https://github.com/Orbdiff/Fileless/releases/download/v1.1/Fileless.exe',
+    'https://github.com/txvch/Screenshare-Collector/releases/download/tech/Technical.Utilities.exe',
+    'https://github.com/ItzIceHere/RedLotusAltChecker/releases/download/RL/RedLotusAltChecker.exe',
+    'https://github.com/Orbdiff/PrefetchView/releases/download/v1.6.3/PrefetchView++.exe',
+    'https://github.com/MeowTonynoh/MeowDoomsdayFucker/releases/download/V.1.1/MeowDoomsdayFucker.exe',
+    'https://dl.echo.ac/tool/journal',
+    'https://github.com/kacos2000/Win10LiveInfo/releases/download/v.1.0.23.0/WinLiveInfo.exe',
+    'https://www.nirsoft.net/utils/regscanner.html',
+    'https://github.com/moaistory/WinSearchDBAnalyzer/releases/download/1.0.0.6/WinSearchDBAnalyzer.exe',
+    'https://www.nirsoft.net/utils/appaudioconfig-x64.zip',
+    'https://github.com/zodiacon/AllTools/raw/master/NtfsStreams.zip',
+    'https://api.anticheat.ac/dl/cli',
+    'https://github.com/Orbdiff/JARParser/releases/download/v1.2/JARParser.exe',
+    'https://github.com/Orbdiff/DPS-Analyzer',
+    'https://github.com/Orbdiff/BAMReveal/releases/download/v1.3/BAMReveal.exe',
+    'https://github.com/Orbdiff/CheckDeletedUSN/releases/download/v0.2.1/CheckDeletedUSN.exe',
+    'https://github.com/Orbdiff/BAM-CheckRestart/releases/download/v2.0.2/BAMCheckRestart.exe'
 
     )
 
